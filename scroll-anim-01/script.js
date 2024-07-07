eece50a1cfa6c67e98923b4008b5cedb52c10bd7
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2024-07-07 14:11:35 -0700

    Update animation on scroll
    Updated the threshold value and added an animation effect triggered by scrolling. The animation changes the opacity of the target element from fully transparent to opaque. It runs on a target element when it intersects with the viewport, and it is removed when it's not intersecting.

--- scroll-anim-01/script.js
+++ scroll-anim-01/script.js
@@ -1,12 +1,18 @@
 let options = {
     root: null,
     rootMargin: "0px",
-    threshold: 0.25
+    //threshold: 0,
+    threshold: 0.5
 }
 
 let callback = (entries, observer) => {
     entries.forEach((entry) => {
         console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("int-target-anim");
+        } else {
+            entry.target.classList.remove("int-target-anim");
+        }
     });
 }
 
