b6e21269dcc7c388622205df679a95a631ff3f4c
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2024-07-13 13:49:24 -0700

    Update navbar styles and interaction
    Adjusted the styles.css to enhance the navbar's appearance and interactivity based on its scrolled state. The changes include the color scheme and addition of transition effects. The script.js is altered to handle the active state of navbar links and to add class toggling based on scrolling position. The interactivity has been enhanced using the IntersectionObserver API for smoother user navigation and better UX.

--- nav-sticky/script.js
+++ nav-sticky/script.js
@@ -1,10 +1,57 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".navbar a");
+
+// This scroll event changes the navbar style depending on whether it's at the top of the document or not
+window.addEventListener("scroll", function() {
+    let navbar = document.querySelector(".navbar");
+
     if (window.scrollY > 0) {
-        navbar.style.backgroundColor = 'rgba(51, 51, 51, 1)';
-        navbar.style.color = "rgba(255, 255, 255, 1)";
+        navbar.classList.add("scrolled");
+        navLinks.forEach(navLink => {
+            navLink.classList.add("scrolled");
+            navLink.classList.remove("un-scrolled");
+        });
     } else {
-        navbar.style.backgroundColor = 'rgba(51, 51, 51, 0)';
-        navbar.style.color = "rgba(51, 51, 51, 1)";
+        navbar.classList.remove("scrolled");
+        navLinks.forEach(navLink => {
+            navLink.classList.remove("scrolled");
+            navLink.classList.add("un-scrolled");
+        });
     }
+});
+
+// This binds a click event to each nav link and handles its active state
+navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", function () {
+       let currentActiveLink = document.querySelector(".active");
+       
+       if (currentActiveLink) {
+           currentActiveLink.classList.remove("active");
+       }
+       
+       if (this.hash !== "#section1") {
+           this.classList.add("active");
+       }
+    });
+});
+
+let sectionIntersectionObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            let currId = entry.target.getAttribute("id");
+            let activeNavLink = document.querySelector(`.navbar a[href="#${currId}"]`);
+            
+            navLinks.forEach((link) => {
+               if (link === activeNavLink && currId !== "section1") {
+                   link.classList.add("active");
+               } else {
+                   link.classList.remove("active");
+               }
+            });
+        }
+    });
+}, {threshold: 0.5});
+
+sections.forEach((section) => {
+    sectionIntersectionObserver.observe(section);
 });
