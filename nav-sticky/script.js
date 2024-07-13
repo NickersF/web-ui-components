ec90853324c3fbbde9421fb3f1b049bcbe1522ca
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2024-07-13 09:45:50 -0700

    Update UI and enhance navbar visibility on scroll
    Removed placeholder text from three sections in the HTML file to declutter the content. The CSS was updated to add color transition, improving the visibility of the navigation bar on scrolling. The JavaScript was modified to change navbar text color when scrolling, enhancing readability.

--- nav-sticky/script.js
+++ nav-sticky/script.js
@@ -1,8 +1,10 @@
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
-    if (window.pageYOffset > 0) {
+    if (window.scrollY > 0) {
         navbar.style.backgroundColor = 'rgba(51, 51, 51, 1)';
+        navbar.style.color = "rgba(255, 255, 255, 1)";
     } else {
         navbar.style.backgroundColor = 'rgba(51, 51, 51, 0)';
+        navbar.style.color = "rgba(51, 51, 51, 1)";
     }
 });
