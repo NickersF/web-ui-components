bcd064eb081c6927517c911d0277dea5852e448b
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2024-11-21 07:49:04 -0800

    Added hiding of toolbar for edit tab

--- text-editor-01/script.js
+++ text-editor-01/script.js
@@ -6,11 +6,20 @@ class TextEditor {
     initializeTextEditor() {
         let editBtn = document.getElementById("editBtn");
         let previewBtn = document.getElementById("previewBtn");
+        let editorToolbar = document.getElementById("editorToolBar");
 
         const toggleTabs = (e) => {
             let tabId = e.target.getAttribute("id").replace("Btn", "Tab");
             let navTabs = document.querySelectorAll(".nav-tab-content");
+            
+            // Toggle the editor controls when the edit tab is active
+            if (tabId === "editTab") {
+                editorToolbar.classList.remove("display-none");
+            } else {
+                editorToolbar.classList.add("display-none");
+            }
 
+            // Toggle the tab
             navTabs.forEach((item) => {
                 if (item.getAttribute("id") !== tabId) {
                     item.classList.add("display-none");
