c8821c1891927d63b991d76da57b874b53944d78
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2024-12-25 12:33:37 -0800

    Dropdown button base interaction completed.

---
+++ button-dd-01/script.js
@@ -0,0 +1,16 @@
+const menuButton1 = document.getElementById("menuButton1");
+const buttonMenu1 = document.getElementById("buttonMenu1");
+
+menuButton1.addEventListener("click", function () {
+    let buttonMenuClassList = buttonMenu1.classList;
+    
+    console.log(buttonMenuClassList);
+    
+    if (buttonMenuClassList.contains("button-menu-container-closed")) {
+        buttonMenuClassList.remove("button-menu-container-closed");
+        buttonMenuClassList.add("button-menu-container-open");
+    } else {
+        buttonMenuClassList.remove("button-menu-container-open");
+        buttonMenuClassList.add("button-menu-container-closed");
+    }
+});
