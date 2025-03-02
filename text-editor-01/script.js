9ac097d8130cc93a73fd941dfabbda065ab177ae
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2025-03-01 23:37:54 -0800

    Added the editing, set up the basic dimensional styling for the controls. Buttons and responsive features next.

--- text-editor-01/script.js
+++ text-editor-01/script.js
@@ -3,6 +3,7 @@ class TextEditor {
         this.rootElId = rootElId;
     }
 
+    // Most of this will be gone when porting the layout to Blazor - I just need to see how this works
     initializeTextEditor() {
         let editBtn = document.getElementById("editBtn");
         let previewBtn = document.getElementById("previewBtn");
@@ -11,12 +12,15 @@ class TextEditor {
         const toggleTabs = (e) => {
             let tabId = e.target.getAttribute("id").replace("Btn", "Tab");
             let navTabs = document.querySelectorAll(".nav-tab-content");
+            let editorTextArea = document.getElementById("editorTextArea");
+            let previewTab = document.getElementById("previewTab");
             
             // Toggle the editor controls when the edit tab is active
             if (tabId === "editTab") {
                 editorToolbar.classList.remove("display-none");
             } else {
                 editorToolbar.classList.add("display-none");
+                previewTab.textContent = editorTextArea.value;
             }
 
             // Toggle the tab
@@ -24,7 +28,7 @@ class TextEditor {
                 if (item.getAttribute("id") !== tabId) {
                     item.classList.add("display-none");
                 } else {
-                    item.classList.remove("display-none"); // Corrected typo here
+                    item.classList.remove("display-none");
                 }
             });
         };
