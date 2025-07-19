d6a487c07112cf934dced6baf2c81fbbfec9738a
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2025-07-19 15:10:10 -0700

    Normalized the CSS to HTML element naming to organize the core layout better.

--- layout-panels-02/script.js
+++ layout-panels-02/script.js
@@ -1,1 +1,8 @@
+export class PanelLayout {
+    test() {
+        console.log("test1");
+    }
+}
+let panelLayout = new PanelLayout();
+panelLayout.test();
 //# sourceMappingURL=script.js.map
---
+++ layout-panels-02/script.ts
@@ -0,0 +1,9 @@
+export class PanelLayout {
+    public test(): void {
+        console.log("test1");
+    }
+}
+
+let panelLayout: PanelLayout = new PanelLayout();
+
+panelLayout.test();
